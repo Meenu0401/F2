f7a24c48f75110a8896186c7e84a5b3b56579f3e
Meenu3112 <gupta_meenu77@yahoo.com>
2024-07-30 15:45:48 +0530

    first push

---
+++ TryingGitHubTP04/src/test/java/P1/B1.java
@@ -0,0 +1,10 @@
+package P1;
+
+import org.testng.annotations.Test;
+
+public class B1 {
+@Test
+public void freeTest() {
+	System.out.println("ggdgddgdggd");
+}
+}
---
+++ TryingGitHubTP04/src/test/java/com/comcast/crm/contact/contacttest.java
@@ -0,0 +1,11 @@
+package com.comcast.crm.contact;
+
+import org.testng.annotations.Test;
+
+public class contacttest {
+
+	@Test
+	public void contactTest(){
+		System.out.println("Execution started");
+	}
+}
